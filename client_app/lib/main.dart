08103344d9933360860b71393b91f6627b35994d
AbdulbasitSaid <mrspeed518@gmail.com>
2025-02-18 15:42:19 +0100

    Integrate gRPC client for Todo service and update UI to display todos

--- client_app/lib/main.dart
+++ client_app/lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:protos/protos.dart';
 
 void main() {
-  runApp(const MainApp());
+  final rpcClientChannel = ClientChannel('10.0.0.169',
+      port: 50051, options: const ChannelOptions(credentials: ChannelCredentials.insecure()));
+
+  final stub = TodoServiceClient(rpcClientChannel);
+  runApp(MainApp(
+    stub: stub,
+  ));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+  const MainApp({super.key, required this.stub});
+  final TodoServiceClient stub;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: StreamBuilder<Object>(
+            stream: stub.listTodosStream(Empty()),
+            builder: (cxt, snapshot) {
+              if (snapshot.hasData) {
+                final todo = snapshot.data;
+                if (todo == null) {
+                  return Center(
+                    child: Text('No data'),
+                  );
+                }
+                todo as Todo;
+                return ListTile(
+                  title: Text(todo.title),
+                  subtitle: Text(todo.description),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }),
       ),
     );
   }
